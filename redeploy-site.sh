faa7ef8ef127bf997a8ca8256aa34071f52d9d54
Mohammad Najm <128323124+Mnajm6201@users.noreply.github.com>
2025-07-13 08:29:17 -0400

    Updated redeploy to deal with new system service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server 2>/dev/null
 
 # Navigate to project directory
 cd ~/portfolio || {
@@ -16,5 +14,6 @@ git fetch && git reset origin/main --hard
 source venv/bin/activate
 pip install -r requirements.txt
 
-# Start new tmux session running the Flask app
-tmux new-session -d -s flask-session "source venv/bin/activate && flask run --host=0.0.0.0"
+# Restart the systemd service
+systemctl daemon-reload
+systemctl restart myportfolio
